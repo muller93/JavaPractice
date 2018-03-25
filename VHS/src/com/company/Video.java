f3b143bc712d84d0c677e6184e966aa7717d5d46
Norbert Muller <mullernorbert93@gmail.com>
2018-03-25 14:55:56 +0200

    .

--- VHS/src/com/company/Video.java
+++ VHS/src/com/company/Video.java
@@ -2,27 +2,66 @@ package com.company;
 
 public class Video implements Loan{
     private String filmTitle;
-    private boolean canLoan;
+    private boolean canLoan = true;
     private String howLongLoan; //lehetséges értékei: „egynapos”, „kétnapos”, „korlátlan”),
     private int ageLimit; //(értéke 0, 12, 16 vagy 18 lehet)
 
     public Video(String filmTitle, String howLongLoan, int ageLimit) {
         this.filmTitle = filmTitle;
-        this.canLoan = true;
-        this.howLongLoan = howLongLoan;
+
+        if (ageLimit == 0 || ageLimit == 12 || ageLimit == 16 || ageLimit == 18){
         this.ageLimit = ageLimit;
+        } else {
+            System.out.println("Wrong age limit value");
+        }
+
+        if (howLongLoan.equals("egynapos") || howLongLoan.equals("kétnapos") || howLongLoan.equals("korlátlan")){
+            this.howLongLoan = howLongLoan;
+        } else {
+            System.out.println("You rentable value.");
+        }
+
     }
 
 
     @Override
-    public boolean rent(int one, int two) throws ErrorLoan {
-        return false;
+    public boolean rent(int age, int renting) throws ErrorLoan {
+        if (renting > 0){
+            int rentToInt = 0;
+            switch (howLongLoan){
+                case "one-day":
+                    renting = 1;
+                    break;
+                case "two-day":
+                    renting = 2;
+                    break;
+                case "unlimited":
+                    renting = renting;
+                    break;
+            }
+            if ((age >= ageLimit) && ())
+        }
     }
 
     @Override
     public boolean bringBack() {
         return false;
     }
+    /*f
+            }
+            if((renterAge >= rating) && (numberOFDaysToRent <= rentableForInt && !isRented)) {
+                isRented = true;
+                return true;
+            } else if (isRented) {
+                return false;
+            } else {
+                throw new RentingErrorException("You are either underage, the video is already rented or cannot be rented for the given interval.");
+            }
+        } else {
+            throw new RentingErrorException("Renting time cannot be 0 or negative.");
+        }
+
+}*/
 }
 /*Írj egy Video osztályt, mely implementálja a Kolcsonozheto interfészt!
 Az osztály a következő adattagokkal rendelkezzen: film címe (szöveges),
