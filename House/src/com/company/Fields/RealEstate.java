085856689c28c09ecb09cd0de06eb166e6b73e02
Norbert Muller <mullernorbert93@gmail.com>
2018-04-06 09:56:52 +0200

    fields

---
+++ House/src/com/company/Fields/RealEstate.java
@@ -0,0 +1,5 @@
+package com.company.Fields;
+
+public class RealEstate {
+}
+/*- Ingatlanközvetítő, itt vehetsz lakást, ára: 50.000 Ft.*/
---
+++ House/src/com/company/Fields/Start.java
@@ -0,0 +1,7 @@
+package com.company.Fields;
+
+public class Start {
+
+}
+/*- Startmező. Ha ezen áthaladsz, 4.000 Ft-ot kapsz fizetésként,
+ha megállsz rajta, 6.000-et. */
