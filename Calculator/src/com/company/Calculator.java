ade8d1193a1dd82f747fbbe45086baa41e8dfb50
Norbert Muller <mullernorbert93@gmail.com>
2018-04-25 14:04:47 +0200

    calculator

---
+++ Calculator/src/com/company/Calculator.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator {
+    private JButton button1;
+    private JButton ACButton;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton xButton;
+    private JButton a0Button;
+    private JButton button10;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a5Button;
+    private JButton a4Button;
+    private JButton a3Button;
+    private JButton a2Button;
+    private JButton a1Button;
+
+    public Calculator() {
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "oké");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new Calculator().a0Button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ Calculator/src/com/company/MainWindowColtroller.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MainWindowColtroller {
+
+    private Main main;
+
+    public MainWindowColtroller(Main main) {
+        this.main = main;
+    }
+}
